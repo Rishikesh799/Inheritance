3dca3f8b4b474ecf17cc18e11861aafc645d687a
Rishikesh Shankar Patil <97303907+Rishikesh799@users.noreply.github.com>
2022-02-24 13:24:15 +0530

    Update inheritance_1.py

--- inheritance_1.py
+++ inheritance_1.py
@@ -1,5 +1,5 @@
 ### Create python class with attributes of name,age and section with inheritance.
-#Create a Python class Person with attributes: name and age of type string.
+#Create a Python class Person with attributes: name and age of type String.
 #Create a display() method that displays the name and age of an object created via the Person class.
 #Create a child class Student  which inherits from the Person class and which also has a section attribute.
 #Create a method displayStudent() that displays the name, age and section of an object created via the Student class.
